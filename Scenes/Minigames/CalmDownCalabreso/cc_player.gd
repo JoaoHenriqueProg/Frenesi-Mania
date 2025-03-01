947a0ea0fba68a1d2cc62d8ef28980dd0610d439
JoaoHenriqueProg <joaohenriquejh.mds@gmail.com>
2025-03-01 16:39:11 -0300

    feat: física melhor e dash no minigame inspirado em cuphead

--- Scenes/Minigames/CalmDownCalabreso/cc_player.gd
+++ Scenes/Minigames/CalmDownCalabreso/cc_player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -750.0
 
 @export var player_controller : int = -1
 
@@ -10,6 +10,8 @@ var global_delta
 func _ready() -> void:
 	assert(player_controller != -1)
 	assert(player_controller < 4)
+
+var dash_timer = 0
 
 func _physics_process(delta: float) -> void:
 	global_delta = delta
@@ -24,10 +26,15 @@ func _physics_process(delta: float) -> void:
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction := Input.get_axis("ui_left", "ui_right")
+		
+		var is_dashing = Input.is_action_just_pressed("B (Xbox)") and dash_timer == 0
+		
 		if direction:
-			velocity.x = direction * SPEED
+			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED)
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		if is_dashing: velocity.x *= 7.5
 	else:
 		apply_gravity()
 		# TODO: ia
@@ -36,4 +43,6 @@ func _physics_process(delta: float) -> void:
 
 func apply_gravity():
 	if not is_on_floor():
-		velocity += get_gravity() * global_delta
+		velocity += get_gravity() * global_delta * 1.25
+		if velocity.y > 0:
+			velocity.y *= 1.1
